4acf08d962afe625e59f9ebd550a8b2294faf4ec
jcwang587 <jiachengwang@umass.edu>
2024-11-18 17:35:40 -0500

    add clean structures

--- 01_process_data/b2_remove_large_mol.py
+++ 01_process_data/b2_remove_large_mol.py
@@ -5,21 +5,17 @@ import os
 import pandas as pd
 from schrodinger.structure import StructureReader, StructureWriter
 import matplotlib.pyplot as plt
-import fnmatch
 
-from a4_table_gen_opt import main as table_gen
 
 import shutil
 import sys
 sys.path.append("../src")
 from utils import renew_folder
 
-# Run the table generation script
-print("Running the SOM csv generation script...")
-table_gen(dataset_path="../data/zaretzki/0_all/3A4.sdf")
 
-# Read the structures
-reader = StructureReader("../data/zaretzki/0_all/3A4.sdf")
+# Load the input dataset
+reader = StructureReader("../data/3A4.sdf")
+dir_mae = "../data/mae"
 
 # Get the number of non-hydrogen atoms in each molecule
 num_atoms = []
@@ -31,57 +27,9 @@ for i, structure in enumerate(reader, start=1):
     num_atoms.append(len(structure.atom))
     molecule_rank.append((i, len(structure.atom)))
 
-# Plot with the distribution plot
-plt.style.use("seaborn-poster")
-plt.figure(figsize=(12, 8))
-
-ax = plt.gca()
-for spine in ax.spines.values():
-    spine.set_linewidth(2)
-
-counts, bins, patches = plt.hist(
-    num_atoms, bins=range(0, 100, 10), color="#08312a", edgecolor="white"
-)
-plt.xlabel("Number of Non-Hydrogen Atoms", fontsize=18)
-plt.ylabel("Frequency", fontsize=18)
-plt.xlim(0, 90)
-plt.grid(True, which="both", linestyle="--", linewidth=0.5)
-
-# Add numbers on top of each bar
-for count, bin in zip(counts, bins):
-    plt.text(
-        bin + 5,
-        count + 0.5,
-        str(int(count)),
-        ha="center",
-        va="bottom",
-        fontsize=16,
-        color="black",
-    )
-
-# Save the plot
-plt.savefig(
-    "../data/zaretzki/0_all/large_molecule.png", dpi=300, bbox_inches="tight"
-)  
-
-# Print the molecule rank with the top 3 largest molecules
-print("The molecules with 0-10 atoms:")
-print(sorted(molecule_rank, key=lambda x: x[1])[:14])
-print("The molecules with 70-90 atoms:")
-print(sorted(molecule_rank, key=lambda x: x[1], reverse=True)[0:3])
-print("The molecules with 60-70 atoms:")
-print(sorted(molecule_rank, key=lambda x: x[1], reverse=True)[3:12])
-print("The molecules with 50-60 atoms:")
-print(sorted(molecule_rank, key=lambda x: x[1], reverse=True)[12:18])
-print("The molecules with 40-50 atoms:")
-print(sorted(molecule_rank, key=lambda x: x[1], reverse=True)[18:36])
-
-# Remove the molecules with more than 50 atoms
-dir_0_all_mae = "../data/zaretzki/0_all/mae_all"
-
-# Get the file names for the molecules with more than 40 atoms and less than 10 atoms
+# Step 1: Get the file names for the molecules with more than 40 atoms and less than 10 atoms
 file_names = []
-for f in os.listdir(dir_0_all_mae):
+for f in os.listdir(dir_mae):
     idx = int(f.split("_")[1].split(".")[0])
     if idx in [
         m[0] for m in sorted(molecule_rank, key=lambda x: x[1], reverse=True)[:36]
@@ -90,9 +38,9 @@ for f in os.listdir(dir_0_all_mae):
     if idx in [m[0] for m in sorted(molecule_rank, key=lambda x: x[1])[:14]]:
         file_names.append(f)
 
-# Remove the structures including uncommon atoms
+# Step 2: Get the file names for the molecules with uncommon atoms
 uncommon_atoms_file_names = []
-reader = StructureReader("../data/zaretzki/0_all/3A4.sdf")
+reader = StructureReader("../data/3A4.sdf")
 for i, structure in enumerate(reader, start=1):
     for atom in structure.atom:
         if atom.element not in ["H", "C", "N", "O", "S", "F", "P", "Cl", "Br"]:
@@ -102,11 +50,11 @@ for i, structure in enumerate(reader, start=1):
 print(f"Structures with uncommon atoms: {uncommon_atoms_file_names}")
 file_names.extend(uncommon_atoms_file_names)
 
-# Remove the structures including SOM connected to a peroxide
-som_df = pd.read_csv('../data/zaretzki/0_all/3a4_all_som.csv')
+# Step 3: Get the file names for the molecules with peroxides
+som_df = pd.read_csv('../data/3a4_all_som.csv')
 
 peroxide_file_names = []
-reader = StructureReader("../data/zaretzki/0_all/3A4.sdf")
+reader = StructureReader("../data/3A4.sdf")
 for i, structure in enumerate(reader, start=1):
     for atom in structure.atom:
         if atom.index in som_df[som_df['mol_index'] == i]['som'].values:
@@ -139,3 +87,26 @@ print(f"Structures with peroxides: {peroxide_file_names}")
 file_names.extend(peroxide_file_names)
 
 print(f"The files to be removed: {file_names}")
+
+# Copy the remaining files to the new folder
+renew_folder("../data/mae_c_clean")
+renew_folder("../data/mae_n_clean")
+renew_folder("../data/mae_s_clean")
+
+# Carbon
+carbon_file_names = os.listdir("../data/mae_c")
+for file_name in carbon_file_names:
+    if file_name not in file_names:
+        shutil.copy(f"../data/mae_c/{file_name}", f"../data/mae_c_clean/{file_name}")
+
+# Nitrogen
+nitrogen_file_names = os.listdir("../data/mae_n")
+for file_name in nitrogen_file_names:
+    if file_name not in file_names:
+        shutil.copy(f"../data/mae_n/{file_name}", f"../data/mae_n_clean/{file_name}")
+
+# Sulfur
+sulfur_file_names = os.listdir("../data/mae_s")
+for file_name in sulfur_file_names:
+    if file_name not in file_names:
+        shutil.copy(f"../data/mae_s/{file_name}", f"../data/mae_s_clean/{file_name}")
